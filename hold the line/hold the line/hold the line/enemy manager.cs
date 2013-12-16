0e8ffba37249e87f3e9d87f216fb03a1ba836496
twichey <bhawley321@gmail.com>
2013-12-16 08:23:36 -0500

    all classes made

---
+++ hold the line/hold the line/hold the line/enemy manager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace hold_the_line
+{
+    class enemy_maniger
+    {
+    }
+}
---
+++ hold the line/hold the line/hold the line/sprite.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace hold_the_line
+{
+    class sprite
+    {
+        public class Sprite
+        {
+            GraphicsDeviceManager graphics;
+            SpriteBatch spriteBatch;
+
+            Texture2D exploding ;
+            Texture2D turrets;
+        }
+    }
+}
