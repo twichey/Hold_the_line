0e868e8650186079ebbdfc4b26dc5ea027bd2374
twichey <bhawley321@gmail.com>
2013-12-18 10:11:40 -0500

    needs fixing

--- hold the line/hold the line/hold the line/sprite.cs
+++ hold the line/hold the line/hold the line/sprite.cs
@@ -14,8 +14,15 @@ namespace hold_the_line
             GraphicsDeviceManager graphics;
             SpriteBatch spriteBatch;
 
-            Texture2D exploding ;
+            Texture2D exploding;
             Texture2D turrets;
+
+            protected List<Rectangle> frames = new List<Rectangle>();
+            private int frameWidth = 0;
+            private int frameHeight = 0;
+            private int currentFrame;
+            private float frameTime = 0.1f;
+            private float timeForCurrentFrame = 0.0f;
         }
     }
 }
